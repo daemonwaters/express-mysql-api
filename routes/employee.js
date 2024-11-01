509ae08776c436775801d50a441603cea503761f
Ali Mohammadi <chipleader111@gmail.com>
2024-11-01 13:49:12 +0330

    deleted line from non-existing module

--- routes/employee.js
+++ routes/employee.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controller/employee.controller");
-const { body } = require("express-validator");
 
 // GET findAll
 router.get("/", controller.findAll);
